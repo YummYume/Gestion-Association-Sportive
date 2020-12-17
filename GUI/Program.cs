8059f718ba49a18af17a3d51ccc3cd7de98353ad
YummYume <givsoul@gmail.com>
2020-12-17 11:49:20 +0100

    Fix

--- GUI/Program.cs
+++ GUI/Program.cs
@@ -16,10 +16,9 @@ namespace GUI
         static void Main()
         {
             //Instruction de test
-            Adherent unAdherent = new Adherent(0, "nom", "prenom", new DateTime(2001, 4, 18), "Femme", "AAAA", "123", 0598652541, "aaa@aaa.aa", 0598652542, DateTime.Now, false, new Utilisateur(0, "AAAA", "123", "User"), new Classe(0, "ClasseTest"));
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormModificationAdherent(unAdherent));
+            Application.Run(new FormConnexion());
         }
     }
 }
