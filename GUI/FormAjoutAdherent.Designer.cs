c600ea928ef16535427e03b4797a86c93877f525
NicoYam <givsoul@gmail.com>
2020-12-18 12:04:44 +0100

    Merge

--- GUI/FormAjoutAdherent.Designer.cs
+++ GUI/FormAjoutAdherent.Designer.cs
@@ -85,65 +85,73 @@ namespace GUI
             this.flpFormulaire.Controls.Add(this.lsbClasse);
             this.flpFormulaire.Controls.Add(this.lblErrorClasse);
             this.flpFormulaire.Controls.Add(this.btnValider);
-            this.flpFormulaire.Location = new System.Drawing.Point(240, 12);
+            this.flpFormulaire.Location = new System.Drawing.Point(180, 10);
+            this.flpFormulaire.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.flpFormulaire.Name = "flpFormulaire";
-            this.flpFormulaire.Size = new System.Drawing.Size(319, 497);
+            this.flpFormulaire.Size = new System.Drawing.Size(239, 404);
             this.flpFormulaire.TabIndex = 0;
             // 
             // lblNom
             // 
             this.lblNom.AutoSize = true;
-            this.lblNom.Location = new System.Drawing.Point(3, 0);
+            this.lblNom.Location = new System.Drawing.Point(2, 0);
+            this.lblNom.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblNom.Name = "lblNom";
-            this.lblNom.Size = new System.Drawing.Size(37, 17);
+            this.lblNom.Size = new System.Drawing.Size(29, 13);
             this.lblNom.TabIndex = 0;
             this.lblNom.Text = "Nom";
             // 
             // tbxNom
             // 
-            this.tbxNom.Location = new System.Drawing.Point(46, 3);
+            this.tbxNom.Location = new System.Drawing.Point(35, 2);
+            this.tbxNom.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tbxNom.Name = "tbxNom";
-            this.tbxNom.Size = new System.Drawing.Size(100, 22);
+            this.tbxNom.Size = new System.Drawing.Size(76, 20);
             this.tbxNom.TabIndex = 1;
             // 
             // lblErrorNom
             // 
             this.lblErrorNom.ForeColor = System.Drawing.Color.Red;
-            this.lblErrorNom.Location = new System.Drawing.Point(3, 28);
+            this.lblErrorNom.Location = new System.Drawing.Point(2, 24);
+            this.lblErrorNom.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblErrorNom.Name = "lblErrorNom";
-            this.lblErrorNom.Size = new System.Drawing.Size(316, 23);
+            this.lblErrorNom.Size = new System.Drawing.Size(235, 19);
             this.lblErrorNom.TabIndex = 4;
             // 
             // lblPrenom
             // 
             this.lblPrenom.AutoSize = true;
-            this.lblPrenom.Location = new System.Drawing.Point(3, 51);
+            this.lblPrenom.Location = new System.Drawing.Point(2, 43);
+            this.lblPrenom.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblPrenom.Name = "lblPrenom";
-            this.lblPrenom.Size = new System.Drawing.Size(57, 17);
+            this.lblPrenom.Size = new System.Drawing.Size(43, 13);
             this.lblPrenom.TabIndex = 2;
             this.lblPrenom.Text = "Prenom";
             // 
             // tbxPrenom
             // 
-            this.tbxPrenom.Location = new System.Drawing.Point(66, 54);
+            this.tbxPrenom.Location = new System.Drawing.Point(49, 45);
+            this.tbxPrenom.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tbxPrenom.Name = "tbxPrenom";
-            this.tbxPrenom.Size = new System.Drawing.Size(100, 22);
+            this.tbxPrenom.Size = new System.Drawing.Size(76, 20);
             this.tbxPrenom.TabIndex = 3;
             // 
             // lblErrorPrenom
             // 
             this.lblErrorPrenom.ForeColor = System.Drawing.Color.Red;
-            this.lblErrorPrenom.Location = new System.Drawing.Point(3, 79);
+            this.lblErrorPrenom.Location = new System.Drawing.Point(2, 67);
+            this.lblErrorPrenom.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblErrorPrenom.Name = "lblErrorPrenom";
-            this.lblErrorPrenom.Size = new System.Drawing.Size(316, 23);
+            this.lblErrorPrenom.Size = new System.Drawing.Size(235, 19);
             this.lblErrorPrenom.TabIndex = 5;
             // 
             // lblDnd
             // 
             this.lblDnd.AutoSize = true;
-            this.lblDnd.Location = new System.Drawing.Point(3, 102);
+            this.lblDnd.Location = new System.Drawing.Point(2, 86);
+            this.lblDnd.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblDnd.Name = "lblDnd";
-            this.lblDnd.Size = new System.Drawing.Size(126, 17);
+            this.lblDnd.Size = new System.Drawing.Size(96, 13);
             this.lblDnd.TabIndex = 6;
             this.lblDnd.Text = "Date de naissance";
             // 
@@ -151,153 +159,169 @@ namespace GUI
             // 
             this.dtpDn.CustomFormat = "dd/MM/yyyy";
             this.dtpDn.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.dtpDn.Location = new System.Drawing.Point(135, 105);
+            this.dtpDn.Location = new System.Drawing.Point(102, 88);
+            this.dtpDn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dtpDn.Name = "dtpDn";
-            this.dtpDn.Size = new System.Drawing.Size(109, 22);
+            this.dtpDn.Size = new System.Drawing.Size(83, 20);
             this.dtpDn.TabIndex = 24;
             this.dtpDn.Value = new System.DateTime(2020, 12, 14, 0, 0, 0, 0);
             // 
             // lblErrorDnd
             // 
             this.lblErrorDnd.ForeColor = System.Drawing.Color.Red;
-            this.lblErrorDnd.Location = new System.Drawing.Point(3, 130);
+            this.lblErrorDnd.Location = new System.Drawing.Point(2, 110);
+            this.lblErrorDnd.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblErrorDnd.Name = "lblErrorDnd";
-            this.lblErrorDnd.Size = new System.Drawing.Size(316, 23);
+            this.lblErrorDnd.Size = new System.Drawing.Size(235, 19);
             this.lblErrorDnd.TabIndex = 8;
             // 
             // lblSexe
             // 
             this.lblSexe.AutoSize = true;
-            this.lblSexe.Location = new System.Drawing.Point(3, 153);
+            this.lblSexe.Location = new System.Drawing.Point(2, 129);
+            this.lblSexe.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblSexe.Name = "lblSexe";
-            this.lblSexe.Size = new System.Drawing.Size(39, 17);
+            this.lblSexe.Size = new System.Drawing.Size(31, 13);
             this.lblSexe.TabIndex = 9;
             this.lblSexe.Text = "Sexe";
             // 
             // lsbSexe
             // 
             this.lsbSexe.FormattingEnabled = true;
-            this.lsbSexe.ItemHeight = 16;
             this.lsbSexe.Items.AddRange(new object[] {
             "Homme",
             "Femme",
             "Autre"});
-            this.lsbSexe.Location = new System.Drawing.Point(48, 156);
+            this.lsbSexe.Location = new System.Drawing.Point(37, 131);
+            this.lsbSexe.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.lsbSexe.Name = "lsbSexe";
-            this.lsbSexe.Size = new System.Drawing.Size(81, 20);
+            this.lsbSexe.Size = new System.Drawing.Size(62, 17);
             this.lsbSexe.TabIndex = 25;
             // 
             // lblErrorSexe
             // 
             this.lblErrorSexe.ForeColor = System.Drawing.Color.Red;
-            this.lblErrorSexe.Location = new System.Drawing.Point(3, 179);
+            this.lblErrorSexe.Location = new System.Drawing.Point(2, 150);
+            this.lblErrorSexe.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblErrorSexe.Name = "lblErrorSexe";
-            this.lblErrorSexe.Size = new System.Drawing.Size(316, 23);
+            this.lblErrorSexe.Size = new System.Drawing.Size(235, 19);
             this.lblErrorSexe.TabIndex = 11;
             // 
             // lblNumTel
             // 
             this.lblNumTel.AutoSize = true;
-            this.lblNumTel.Location = new System.Drawing.Point(3, 202);
+            this.lblNumTel.Location = new System.Drawing.Point(2, 169);
+            this.lblNumTel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblNumTel.Name = "lblNumTel";
-            this.lblNumTel.Size = new System.Drawing.Size(207, 17);
+            this.lblNumTel.Size = new System.Drawing.Size(155, 13);
             this.lblNumTel.TabIndex = 12;
             this.lblNumTel.Text = "Numéro de téléphone Adherent";
             // 
             // tbxNumTel
             // 
-            this.tbxNumTel.Location = new System.Drawing.Point(216, 205);
+            this.tbxNumTel.Location = new System.Drawing.Point(161, 171);
+            this.tbxNumTel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tbxNumTel.Name = "tbxNumTel";
-            this.tbxNumTel.Size = new System.Drawing.Size(100, 22);
+            this.tbxNumTel.Size = new System.Drawing.Size(76, 20);
             this.tbxNumTel.TabIndex = 13;
             // 
             // lblErrorNumTel
             // 
             this.lblErrorNumTel.ForeColor = System.Drawing.Color.Red;
-            this.lblErrorNumTel.Location = new System.Drawing.Point(3, 230);
+            this.lblErrorNumTel.Location = new System.Drawing.Point(2, 193);
+            this.lblErrorNumTel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblErrorNumTel.Name = "lblErrorNumTel";
-            this.lblErrorNumTel.Size = new System.Drawing.Size(316, 23);
+            this.lblErrorNumTel.Size = new System.Drawing.Size(235, 19);
             this.lblErrorNumTel.TabIndex = 14;
             // 
             // lblEmail
             // 
             this.lblEmail.AutoSize = true;
-            this.lblEmail.Location = new System.Drawing.Point(3, 253);
+            this.lblEmail.Location = new System.Drawing.Point(2, 212);
+            this.lblEmail.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblEmail.Name = "lblEmail";
-            this.lblEmail.Size = new System.Drawing.Size(42, 17);
+            this.lblEmail.Size = new System.Drawing.Size(32, 13);
             this.lblEmail.TabIndex = 15;
             this.lblEmail.Text = "Email";
             // 
             // tbxEmail
             // 
-            this.tbxEmail.Location = new System.Drawing.Point(51, 256);
+            this.tbxEmail.Location = new System.Drawing.Point(38, 214);
+            this.tbxEmail.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tbxEmail.Name = "tbxEmail";
-            this.tbxEmail.Size = new System.Drawing.Size(100, 22);
+            this.tbxEmail.Size = new System.Drawing.Size(76, 20);
             this.tbxEmail.TabIndex = 16;
             // 
             // lblErrorEmail
             // 
             this.lblErrorEmail.ForeColor = System.Drawing.Color.Red;
-            this.lblErrorEmail.Location = new System.Drawing.Point(3, 281);
+            this.lblErrorEmail.Location = new System.Drawing.Point(2, 236);
+            this.lblErrorEmail.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblErrorEmail.Name = "lblErrorEmail";
-            this.lblErrorEmail.Size = new System.Drawing.Size(316, 23);
+            this.lblErrorEmail.Size = new System.Drawing.Size(235, 19);
             this.lblErrorEmail.TabIndex = 17;
             // 
             // lblNumTelParent
             // 
             this.lblNumTelParent.AutoSize = true;
-            this.lblNumTelParent.Location = new System.Drawing.Point(3, 304);
+            this.lblNumTelParent.Location = new System.Drawing.Point(2, 255);
+            this.lblNumTelParent.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblNumTelParent.Name = "lblNumTelParent";
-            this.lblNumTelParent.Size = new System.Drawing.Size(191, 17);
+            this.lblNumTelParent.Size = new System.Drawing.Size(143, 13);
             this.lblNumTelParent.TabIndex = 18;
             this.lblNumTelParent.Text = "Numéro de téléphone Parent";
             // 
             // tbxNumTelParent
             // 
-            this.tbxNumTelParent.Location = new System.Drawing.Point(200, 307);
+            this.tbxNumTelParent.Location = new System.Drawing.Point(149, 257);
+            this.tbxNumTelParent.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tbxNumTelParent.Name = "tbxNumTelParent";
-            this.tbxNumTelParent.Size = new System.Drawing.Size(100, 22);
+            this.tbxNumTelParent.Size = new System.Drawing.Size(76, 20);
             this.tbxNumTelParent.TabIndex = 19;
             // 
             // lblErrorNumTelParent
             // 
             this.lblErrorNumTelParent.ForeColor = System.Drawing.Color.Red;
-            this.lblErrorNumTelParent.Location = new System.Drawing.Point(3, 332);
+            this.lblErrorNumTelParent.Location = new System.Drawing.Point(2, 279);
+            this.lblErrorNumTelParent.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblErrorNumTelParent.Name = "lblErrorNumTelParent";
-            this.lblErrorNumTelParent.Size = new System.Drawing.Size(316, 23);
+            this.lblErrorNumTelParent.Size = new System.Drawing.Size(235, 19);
             this.lblErrorNumTelParent.TabIndex = 20;
             // 
             // lblClasse
             // 
             this.lblClasse.AutoSize = true;
-            this.lblClasse.Location = new System.Drawing.Point(3, 355);
+            this.lblClasse.Location = new System.Drawing.Point(2, 298);
+            this.lblClasse.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblClasse.Name = "lblClasse";
-            this.lblClasse.Size = new System.Drawing.Size(50, 17);
+            this.lblClasse.Size = new System.Drawing.Size(38, 13);
             this.lblClasse.TabIndex = 21;
             this.lblClasse.Text = "Classe";
             // 
             // lsbClasse
             // 
             this.lsbClasse.FormattingEnabled = true;
-            this.lsbClasse.ItemHeight = 16;
-            this.lsbClasse.Location = new System.Drawing.Point(59, 358);
+            this.lsbClasse.Location = new System.Drawing.Point(44, 300);
+            this.lsbClasse.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.lsbClasse.Name = "lsbClasse";
-            this.lsbClasse.Size = new System.Drawing.Size(81, 20);
+            this.lsbClasse.Size = new System.Drawing.Size(62, 17);
             this.lsbClasse.TabIndex = 26;
             // 
             // lblErrorClasse
             // 
             this.lblErrorClasse.ForeColor = System.Drawing.Color.Red;
-            this.lblErrorClasse.Location = new System.Drawing.Point(3, 381);
+            this.lblErrorClasse.Location = new System.Drawing.Point(2, 319);
+            this.lblErrorClasse.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblErrorClasse.Name = "lblErrorClasse";
-            this.lblErrorClasse.Size = new System.Drawing.Size(316, 23);
+            this.lblErrorClasse.Size = new System.Drawing.Size(235, 19);
             this.lblErrorClasse.TabIndex = 23;
             // 
             // btnValider
             // 
-            this.btnValider.Location = new System.Drawing.Point(3, 407);
+            this.btnValider.Location = new System.Drawing.Point(2, 340);
+            this.btnValider.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnValider.Name = "btnValider";
-            this.btnValider.Size = new System.Drawing.Size(316, 69);
+            this.btnValider.Size = new System.Drawing.Size(237, 56);
             this.btnValider.TabIndex = 27;
             this.btnValider.Text = "Ajouter";
             this.btnValider.UseVisualStyleBackColor = true;
@@ -306,10 +330,11 @@ namespace GUI
             // FormAjoutAdherent
             // 
             this.AcceptButton = this.btnValider;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 521);
+            this.ClientSize = new System.Drawing.Size(600, 423);
             this.Controls.Add(this.flpFormulaire);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "FormAjoutAdherent";
             this.Text = "FormAjoutAdherent";
             this.flpFormulaire.ResumeLayout(false);
